f6746c49f3300f6fcfea9bebc90f972a75084c14
Alejandro Rodriguez Muñoz <arodriguem@eafit.edu.co>
2023-09-14 03:37:25 -0500

    Correcion errores

--- N2/Nodo.py
+++ N2/Nodo.py
@@ -1,3 +1,4 @@
+import base64
 import multiprocessing
 import socket, struct, pickle
 from TablaValores import tabla_valores
@@ -41,8 +42,7 @@ class nodo():
                 # Recibir datos del cliente.
                 lengthbuf = self.recvall(self.connection, 4)
                 length, = struct.unpack('!I', lengthbuf)
-                msg = self.recvall(self.connection, length)              
-                # self.conn.sendall(b'Se han recibido los datos')    
+                msg = self.recvall(self.connection, length)    
                 self.organizar_datos(msg)
 
             except Exception as e:
@@ -61,7 +61,6 @@ class nodo():
     def organizar_datos(self, msg):
         #Se organiza la informacion
         msg = pickle.loads(msg)
-
         if(msg['operacion'] == '1'):
             self.crear(msg)
         elif(msg['operacion'] == '2'):
@@ -72,25 +71,20 @@ class nodo():
             self.eliminar(msg)
             
     def crear(self, msg):
-        aux = {
-            'llave':msg['llave'],
-            'parte_numero':msg['parte_numero'],
-            'valor':msg['parte_contenido']
-        }
 
-        print(aux)
+        contenido = base64.b64encode(msg['parte_contenido']).decode('utf-8')
+
+        aux = {
+            msg['llave']: {
+                'parte_numero': msg['parte_numero'],
+                'valor': contenido
+                }
+            }
         
         t = tabla_valores()
         t.inicializar_tabla()
         t.crear_llave(aux)
         t.guardar_llaves()
-        
-    def leer(self, msg):
-        t = tabla_valores()
-        t.inicializar_tabla()
-        msg = t.leer_llave(msg['llave'])
-         
-        self.enviar(msg)
         
     def actualizar(self, msg):
         t = tabla_valores()
@@ -104,20 +98,28 @@ class nodo():
         t.eliminar(msg['llave'])
         t.guardar_llaves()
         
+    def leer(self, msg):
+        t = tabla_valores()
+        t.inicializar_tabla()
+        llave = msg['llave']
+
+        contenido = t.buscar_por_llave(llave)      
+        contenido["valor"] = base64.b64decode(contenido["valor"].encode('utf-8'))
+
+        self.enviar(contenido)
 
     def enviar(self, msg):
-        #Enviar datos al nodo  
-        print('Enviando datos')
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connection.connect((self.hostname, 4999))
+        # Enviar datos al balanceador utilizando la conexión existente
+        print('Enviando datos al balanceador')
+        msg_serializado = pickle.dumps(msg)
+        length = len(msg_serializado)
 
-        msg = pickle.dumps(msg)
-        length = len(msg)
-        
-        connection.sendall(struct.pack('!I', length))
-        connection.sendall(msg)
-        connection.close()
-        
+        try:
+            self.connection.sendall(struct.pack('!I', length))
+            self.connection.sendall(msg_serializado)
+        except Exception as e:
+            print(f"Error al enviar datos al balanceador: {e}")
+            
     
 if __name__ == "__main__":
     s = nodo( hostname = 'localhost', port = 2022)
